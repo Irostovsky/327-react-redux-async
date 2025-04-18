4780036f70e680100252629be0d018f281dbf07a
Irostovsky <ivan.rostovsky@gmail.com>
2025-04-18 14:03:58 +0300

    show/hide the cart

--- src/components/Layout/MainHeader.js
+++ src/components/Layout/MainHeader.js
@@ -1,14 +1,21 @@
-import CartButton from '../Cart/CartButton';
-import classes from './MainHeader.module.css';
+import CartButton from "../Cart/CartButton";
+import classes from "./MainHeader.module.css";
+import { useDispatch } from "react-redux";
+import { cartActions } from "../../store/cart";
 
 const MainHeader = (props) => {
+  const dispatch = useDispatch();
+  const cartButtonHandler = () => {
+    dispatch(cartActions.toggle());
+  };
+
   return (
     <header className={classes.header}>
       <h1>ReduxCart</h1>
       <nav>
         <ul>
           <li>
-            <CartButton />
+            <CartButton onClick={cartButtonHandler} />
           </li>
         </ul>
       </nav>
